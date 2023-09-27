d2e5aa9f2ec19da1cd4da62ff48a18e1ffa30434
Jolka <firjola@gmail.com>
2023-09-27 20:58:41 +0200

    Updates

---
+++ precache-manifest.c3bf3d81f60c826f43aba9da244050ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed671f3811c9e7a64f0f91ae1d4aef3e",
+    "url": "/react-countries/index.html"
+  },
+  {
+    "revision": "921e3ee9e8dc61a583ec",
+    "url": "/react-countries/static/css/main.1141dec0.chunk.css"
+  },
+  {
+    "revision": "25c5b6c4666489dd8b6b",
+    "url": "/react-countries/static/js/2.f967c2f5.chunk.js"
+  },
+  {
+    "revision": "921e3ee9e8dc61a583ec",
+    "url": "/react-countries/static/js/main.fde79ff1.chunk.js"
+  },
+  {
+    "revision": "50d3006fbd2affc08da6",
+    "url": "/react-countries/static/js/runtime~main.d3426653.js"
+  }
+]);
